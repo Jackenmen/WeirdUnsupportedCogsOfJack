3d4f38a1713031c4a4f6c20491839d255f165375
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-11-30 20:48:27 +0100

    [SmileySend] Add support for replies and add more smileys

--- smileysend/smileysend.py
+++ smileysend/smileysend.py
@@ -1,6 +1,7 @@
 import asyncio
 import contextlib
 import functools
+import itertools
 import random
 from typing import List, Iterable
 
@@ -13,6 +14,17 @@ from redbot.core.config import Config
 from redbot.core.utils.chat_formatting import box
 from redbot.core.utils.predicates import MessagePredicate
 
+
+MSG_IDS = "MSG_IDS"
+CONFIG = Config.get_conf(
+    None, 176070082584248320, cog_name="SmileySend", force_registration=True
+)
+CONFIG.register_global(toggle=False, toggle_interactive=False, toggle_replies=False)
+# {CHANNEL_ID: {EMOJI_STR: {message_id: 123}}}
+CONFIG.init_custom(MSG_IDS, 2)
+CONFIG.register_custom(MSG_IDS, message_id=None)
+CONFIG_CACHE = {}
+REPLIES_ENABLED = False
 
 real_send = Messageable.send
 real_send_interactive = Context.send_interactive
@@ -35,7 +47,69 @@ FULL_MORE_LIST = MORE_LIST + [
 ]
 
 
-if discord.version_info[:2] >= (1, 4):
+if discord.version_info[:2] >= (1, 6):
+    async def get_msg_ref(
+        messageable: Messageable, emoji: str
+    ) -> discord.MessageReference:
+        channel = await messageable._get_channel()
+        ref_data = CONFIG_CACHE.setdefault(str(channel.id), {}).setdefault(emoji, {})
+        ref_msg_id = ref_data.get("message_id")
+
+        if ref_msg_id is None:
+            emoji_count = (2000 + 1) // (len(emoji) + 1)
+            ref_msg_content = " ".join(itertools.repeat(emoji, min(100, emoji_count)))
+            ref_msg = await real_send(messageable, ref_msg_content)
+
+            ref_data["message_id"] = ref_msg_id = ref_msg.id
+            scope = CONFIG.custom(MSG_IDS, str(channel.id), emoji).message_id
+            await scope.set(ref_msg_id)
+
+        return discord.MessageReference(message_id=ref_msg_id, channel_id=channel.id)
+
+    @functools.wraps(real_send)
+    async def send(
+        self,
+        content=None,
+        *,
+        tts=False,
+        embed=None,
+        file=None,
+        files=None,
+        delete_after=None,
+        nonce=None,
+        allowed_mentions=None,
+        reference=None,
+        mention_author=None,
+    ):
+        if isinstance(self, Context):
+            emojis = SPECIAL_AUTHOR_CASES.get(self.author.id, OMEGA)
+        else:
+            emojis = OMEGA
+        emoji = random.choice(emojis)
+        content = str(content) if content is not None else None
+        if content:
+            if len(content) >= (2000 - (len(emoji) + 1) * 2):
+                await real_send(self, emoji)
+            else:
+                content = f"{emoji} {content} {emoji}"
+        else:
+            content = emoji
+        if reference is None and REPLIES_ENABLED:
+            reference = await get_msg_ref(self, emoji)
+        return await real_send(
+            self,
+            content,
+            tts=tts,
+            embed=embed,
+            file=file,
+            files=files,
+            delete_after=delete_after,
+            nonce=nonce,
+            allowed_mentions=allowed_mentions,
+            reference=reference,
+            mention_author=mention_author,
+        )
+elif discord.version_info[:2] >= (1, 4):
     @functools.wraps(real_send)
     async def send(
         self,
@@ -178,15 +252,17 @@ async def send_interactive(
 class SmileySend(commands.Cog):
     def __init__(self, bot: Red) -> None:
         self.bot = bot
-        self.config = Config.get_conf(self, 176070082584248320, force_registration=True)
-        self.config.register_global(toggle=False, toggle_interactive=False)
 
     async def initialize(self) -> None:
-        settings = await self.config.all()
+        settings = await CONFIG.all()
         if settings["toggle"]:
             setattr(Messageable, "send", send)
         if settings["toggle_interactive"]:
             setattr(Context, "send_interactive", send_interactive)
+        global CONFIG_CACHE
+        CONFIG_CACHE = await CONFIG.custom(MSG_IDS).all()
+        global REPLIES_ENABLED
+        REPLIES_ENABLED = settings["toggle_replies"]
 
     def cog_unload(self) -> None:
         setattr(Messageable, "send", real_send)
@@ -198,7 +274,7 @@ class SmileySend(commands.Cog):
             setattr(Messageable, "send", send)
         else:
             setattr(Messageable, "send", real_send)
-        await self.config.toggle.set(toggle)
+        await CONFIG.toggle.set(toggle)
         await ctx.tick()
 
     @smileysend.command(name="interactive")
@@ -207,5 +283,12 @@ class SmileySend(commands.Cog):
             setattr(Context, "send_interactive", send_interactive)
         else:
             setattr(Context, "send_interactive", real_send_interactive)
-        await self.config.toggle_interactive.set(toggle)
+        await CONFIG.toggle_interactive.set(toggle)
+        await ctx.tick()
+
+    @smileysend.command(name="replies")
+    async def smileysend_replies(self, ctx: commands.Context, toggle: bool) -> None:
+        global REPLIES_ENABLED
+        REPLIES_ENABLED = toggle
+        await CONFIG.toggle_replies.set(toggle)
         await ctx.tick()
