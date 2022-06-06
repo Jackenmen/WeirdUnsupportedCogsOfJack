3372e06b861517a22b278e03e8743e5f547b5c89
jack1142 <6032823+jack1142@users.noreply.github.com>
2022-06-06 17:46:17 +0200

    [tiv] fix delegate's compatibility with properties, sort of

--- tiv/delegate.py
+++ tiv/delegate.py
@@ -3,8 +3,10 @@ from typing import Any
 
 
 def delegate(cls: type, attr_name: str) -> Any:
+    meth_or_property = getattr(cls, attr_name)
+
     @property
-    @functools.wraps(getattr(cls, attr_name))
+    @functools.wraps(getattr(meth_or_property, "fget", meth_or_property))
     def func(self) -> Any:
         return getattr(cls, attr_name).__get__(self, type(self))
 
