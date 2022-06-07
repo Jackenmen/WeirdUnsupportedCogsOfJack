5eb25f72970b4b6ca19d7ceeee80ec7c3d2576a5
jack1142 <6032823+jack1142@users.noreply.github.com>
2022-06-07 18:55:47 +0200

    [tiv] use `attr_name` for func.__name__

--- tiv/delegate.py
+++ tiv/delegate.py
@@ -5,9 +5,10 @@ from typing import Any
 def delegate(cls: type, attr_name: str) -> Any:
     meth_or_property = getattr(cls, attr_name)
 
-    @property
     @functools.wraps(getattr(meth_or_property, "fget", meth_or_property))
     def func(self) -> Any:
         return getattr(cls, attr_name).__get__(self, type(self))
 
-    return func
+    func.__name__ = attr_name
+
+    return property(func)
