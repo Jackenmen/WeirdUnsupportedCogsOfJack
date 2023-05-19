4b9fed1d85ee54deb03bcf3ca892a3e13003e050
Jakub Kuczys <me@jacken.men>
2023-05-19 19:55:42 +0200

    [headersend] allow skipping code blocks

--- headersend/core.py
+++ headersend/core.py
@@ -1,5 +1,6 @@
 import functools
 import random
+import re
 from typing import Any, List, Optional
 
 import discord
@@ -17,8 +18,16 @@ else:
     from redbot.core.commands import Literal
 
 
+CODE_BLOCK_RE = re.compile(r"```[\w.+\-]+(?!```).*?```|```\n*.+?```")
 real_send = Messageable.send
+SKIP_CODE_BLOCKS = False
 HEADER_SIZE: Optional[int] = 1
+
+
+class ContentPart:
+    def __init__(self, content: str, *, can_split: bool = True) -> None:
+        self.content = content
+        self.can_split = can_split
 
 
 @functools.wraps(real_send)
@@ -29,25 +38,52 @@ async def send(
 ) -> discord.Message:
     content = str(content) if content is not None else None
     if content:
+        content_parts = []
+        if SKIP_CODE_BLOCKS:
+            start = 0
+            end = 0
+            for match in CODE_BLOCK_RE.finditer(content):
+                end = match.start()
+                if end != start:
+                    content_parts.append(ContentPart(content[start:end]))
+                content_parts.append(ContentPart(match.group(0), can_split=False))
+                start = match.end()
+            if start != len(content):
+                content_parts.append(ContentPart(content[start:]))
+        else:
+            content_parts.append(ContentPart(content))
         current_page_length = 0
         pages = []
         lines: List[str] = []
-        for line in content.splitlines():
-            pagifier = pagify(line, [" "], shorten_by=0)
-            header_size = random.randint(1, 3) if HEADER_SIZE is None else HEADER_SIZE
-            pagifier._page_length = 1999 - header_size
-            for longest_line in pagifier:
-                new_line = f"{'#'*header_size} {longest_line}"
+        for part in content_parts:
+            if not part.can_split:
+                new_line = part.content
                 if current_page_length + len(new_line) > 2001:
                     current_page_length = 0
                     pages.append("\n".join(lines))
                     lines.clear()
                 current_page_length += len(new_line) + 1
                 lines.append(new_line)
+                break
+
+            for line in part.content.splitlines():
+                pagifier = pagify(line, [" "], shorten_by=0)
                 header_size = (
                     random.randint(1, 3) if HEADER_SIZE is None else HEADER_SIZE
                 )
-                pagifier._page_length = 1999 - header_size  # DEP-WARN
+                pagifier._page_length = 1999 - header_size
+                for longest_line in pagifier:
+                    new_line = f"{'#'*header_size} {longest_line}"
+                    if current_page_length + len(new_line) > 2001:
+                        current_page_length = 0
+                        pages.append("\n".join(lines))
+                        lines.clear()
+                    current_page_length += len(new_line) + 1
+                    lines.append(new_line)
+                    header_size = (
+                        random.randint(1, 3) if HEADER_SIZE is None else HEADER_SIZE
+                    )
+                    pagifier._page_length = 1999 - header_size  # DEP-WARN
         if lines:
             pages.append("\n".join(lines))
         try:
@@ -67,6 +103,7 @@ class HeaderSend(commands.Cog):
         self.config = Config.get_conf(self, 176070082584248320, force_registration=True)
         self.config.register_global(
             toggle=False,
+            skip_code_blocks=True,
             header_size=1,
         )
 
@@ -74,6 +111,8 @@ class HeaderSend(commands.Cog):
         settings = await self.config.all()
         if settings["toggle"]:
             setattr(Messageable, "send", send)
+        global SKIP_CODE_BLOCKS
+        SKIP_CODE_BLOCKS = settings["skip_code_blocks"]
         global HEADER_SIZE
         HEADER_SIZE = settings["header_size"]
 
@@ -104,4 +143,14 @@ class HeaderSend(commands.Cog):
         global HEADER_SIZE
         HEADER_SIZE = int(size) if size != "random" else None
         await self.config.header_size.set(HEADER_SIZE)
+        await ctx.tick()
+
+    @commands.is_owner()
+    @headersend.command("skipcodeblocks")
+    async def headersend_skipcodeblocks(
+        self, ctx: commands.Context, toggle: bool
+    ) -> None:
+        global SKIP_CODE_BLOCKS
+        SKIP_CODE_BLOCKS = toggle
+        await self.config.skip_code_blocks.set(toggle)
         await ctx.tick()
